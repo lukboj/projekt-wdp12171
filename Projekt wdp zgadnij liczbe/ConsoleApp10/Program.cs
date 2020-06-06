0bf9bf80f1b650516c82ba6505130cce894f7b51
lukboj <65923207+lukboj@users.noreply.github.com>
2020-06-06 04:13:58 +0200

    Update Program.cs

--- Projekt wdp zgadnij liczbe/ConsoleApp10/Program.cs
+++ Projekt wdp zgadnij liczbe/ConsoleApp10/Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices.ComTypes;
+using System.Xml;
 
 namespace ConsoleApp10
 {
@@ -7,39 +8,54 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
-            
+            string zgoda;
             int liczbaprob = 0;
             int liczba;
-            string napiskoncowy = "Łatwym" ;
+            string napiskoncowy = "Łatwym";
             int iloscprob = 5;
             int maxliczba = 100;
             Console.WriteLine("Witam w prostej grze zgadnij liczbe ");
+        gra:
             Console.WriteLine("Prosze wybrać poziom trudności");
             Console.WriteLine("Dostępne są 3 poziomy trudności \n Łatwy \n Średni \n Trudny");
-            Console.WriteLine("Jeśli chcesz wybrać łatwy wpisz 1 \nJeśli chcesz wybrać średni wpisz 2 \nJeśli chcesz wybrać trudny wpisz 3 ");
-             int wybor = int.Parse(Console.ReadLine());
-            if (wybor == 1)
+            Console.WriteLine("Jeśli chcesz wybrać łatwy wpisz Łatwy \nJeśli chcesz wybrać średni wpisz Średni \nJeśli chcesz wybrać trudny wpisz Trudny\n Jeżeli chcesz wybrać ilość prób oraz zakres wpisz Własny ");
+            string wybor = Console.ReadLine();
+        powrot1:
+            if (wybor == "Łatwy")
             {
                 iloscprob = 6;
                 maxliczba = 30;
                 napiskoncowy = "Łatwym";
             }
-            else if (wybor == 2)
+            else if (wybor == "Średni")
             {
                 iloscprob = 5;
                 maxliczba = 60;
                 napiskoncowy = "Średnim";
             }
-            else if (wybor == 3)
+            else if (wybor == "Trudny")
             {
-                iloscprob = 4;
+                iloscprob = 5;
                 maxliczba = 100;
                 napiskoncowy = "Trundym";
+            }
+            else if (wybor == "Własny")
+            {
+                Console.WriteLine("Podaj ile chcesz mieć prób");
+                iloscprob = int.Parse(Console.ReadLine());
+                Console.WriteLine("Napisz do jakiej liczby ma być zakres ");
+                maxliczba = int.Parse(Console.ReadLine());
+                napiskoncowy = "Własnym";
+            }
+            else
+            {
+                Console.WriteLine("Prosze wpisać Łatwy, Średni, Trudny bądź Własny");
+                goto powrot1;
             }
             Random nieznana = new Random();
             
             int los = nieznana.Next(1, maxliczba);
-            Console.WriteLine("Podaj liczbe z zakresu od 1 do "+ maxliczba + ", masz " + iloscprob + " próby !");
+            Console.WriteLine("Podaj liczbe z zakresu od 1 do "+ maxliczba + ", masz " + iloscprob + " prób !");
 
 
             do
@@ -49,7 +65,7 @@ namespace ConsoleApp10
                  liczbaprob++ ;
                  if (iloscprob == liczbaprob)
                 {
-                    Console.WriteLine("Przykro mi , straciłeś wszystkie próby i przegrałeś gre :(");
+                    Console.WriteLine("Przykro mi , straciłeś wszystkie próby i przegrałeś gre ");
                     break;
                 }
                 else if (liczba > los)
@@ -67,6 +83,27 @@ namespace ConsoleApp10
                     Console.WriteLine("Gratulacje udało ci się odgadnąć liczbe na poziomie "+ napiskoncowy + "! Zajeło ci to "+ liczbaprob + " prób");
                 }
             } while (los != liczba);
+            Console.WriteLine("Czy chcesz zagrać ponownie ?");
+            Console.WriteLine("Wpisz Tak albo nie");
+            
+            powrot:
+            zgoda = Console.ReadLine();
+            if (zgoda == "Tak")
+            {
+                goto gra;
+
+            }
+            else if (zgoda == "Nie")
+            {
+                Console.WriteLine("Dziękuje za gre");
+                goto Koniec;
+            }
+            else
+            {
+                Console.WriteLine("Prosze wpisać Tak lub Nie");
+                goto powrot;
+            }
+        Koniec:
             Console.ReadLine();
         }
     }
